c8e8af0117d71415a1f2638ef344def96a5892bc
eewilliac <eewilliac@gmail.com>
2015-10-20 23:27:15 -0400

    Tree and TreeNode

---
+++ TreeLib/TreeLib/TreeNode.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using eListLib;
+
+namespace TreeLib {
+    class TreeNode<T> {
+        private T _data;
+        private List<TreeNode<T>> _children;
+        private TreeNode<T> _parent;
+
+        public TreeNode(TreeNode<T> cparent, T data) {
+            //do something here.
+            if (cparent != null) {
+                cparent.Children.add(this);
+            }
+            this.Data = data;
+        }
+
+        public T Data {
+            get {
+                return _data;
+            }
+            set {
+                _data = value;
+            }
+        }
+
+        public List<TreeNode<T>> Children {
+            get {
+                return _children;
+            }
+            set {
+                _children = value;
+            }
+        }
+
+        public TreeNode<T> Parent {
+            get {
+                return _parent;
+            }
+            set {
+                _parent = value;
+            }
+        }
+    }
+}
